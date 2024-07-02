b64549b36c5aef928fecee6360241ecd4acf441a
Velilla11 <127157179+Velilla11@users.noreply.github.com>
2024-07-02 14:49:20 -0500

    Update compras.js

--- JS/compras.js
+++ JS/compras.js
@@ -23,9 +23,9 @@ function procesarCompra(e){
             text: 'No hay productos, selecciona alguno',
             timer: 2500,
             showConfirmButton: false
-          }).then(function(){
-              window.location = "productos.html";
-          })
+        }).then(function(){
+            window.location = "productos.html";
+        });
     }
     else if(cliente.value === '' || correo.value === ''){
         Swal.fire({
@@ -34,44 +34,17 @@ function procesarCompra(e){
             text: 'Ingrese todos los campos requeridos',
             timer: 2500,
             showConfirmButton: false
-          })
+        });
     }
     else{
-        emailjs.init('user_hx2rYaxbexZ0qlT8bs771')
-
-        const btn = document.getElementById('procesar-compra');
-
-        document.getElementById('procesar-pago')
-        .addEventListener('submit', function(event) {
-        event.preventDefault();
-
-            const cargandoGif = document.querySelector('#cargando');
-            cargandoGif.style.display='block';
-
-            const enviado = document.createElement('img');
-            enviado.src = 'assets/img/mail.gif';
-            enviado.style.display = 'block';
-            enviado.width = '150';
-
-        const serviceID = 'default_service';
-        const templateID = 'template_rtfpoq5';
-
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-            
-                    cargandoGif.style.display = 'none';
-                    document.querySelector('#loaders').appendChild(enviado);
-                    setTimeout(() => {
-                        enviado.remove();
-                        compra.vaciarLocalStorage();
-                        window.location = "productos.html";
-                    }, 2500);
-
-            }, (err) => {
-            btn.value = 'Send Email';
-            alert(JSON.stringify(err));
-            });
-        });
-
+        // Si se cumplen todas las condiciones, muestra el alert de éxito
+        alert('La compra se realizó con éxito');
+        
+        // Vaciar el LocalStorage y redirigir a la página de productos
+        compra.vaciarLocalStorage();
+        window.location = "productos.html";
     }
 }
+
+// Asigna el evento al botón de procesar compra
+document.getElementById('procesar-compra').addEventListener('click', procesarCompra);
